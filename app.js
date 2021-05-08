534777f7d8e9fd0f3d031aefd91548187aeb4d19
Yu-Ting Wu <a22627046@gmail.com>
2021-05-08 19:16:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant_menu_CRUD', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -32,5 +46,5 @@ app.get('/search', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+  console.log(`App is running on http://localhost:${port}`)
 })
