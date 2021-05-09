bd2588fa7e6acef92bf4a07e512aa1af494e6016
Yu-Ting Wu <a22627046@gmail.com>
2021-05-09 23:42:15 +0800

    feat: add update rest function

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ app.get('/restaurants/new', (req, res) => {
 })
 
 app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  return Restaurant.create({ name })
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
@@ -53,6 +53,38 @@ app.get('/restaurants/:id', (req, res) => {
     .then(restaurant => res.render('detail', { restaurant }))
     .catch(error => console.log(error))
 })
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+
+
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
