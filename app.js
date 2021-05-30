9dd8f1be822b80cae927086c728c69f0e9e01baa
Yu-Ting Wu <a22627046@gmail.com>
2021-05-30 18:48:41 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override')
 const helpers = require('handlebars-helpers')()
 // 引用路由器
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
@@ -25,6 +26,9 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 // 將 request 導入路由器
 app.use(routes)
 
